f6582ba8a7ef48337b8e06a498b66410287320a7
Ben Nguyen <bennguyenn24@gmail.com>
2023-01-23 19:26:48 -0800

    Added conditional rendering

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+import { useState } from "react";
 
 const Start = () => {
+	// Show Available Madlibz
+	const [show, setShow] = useState(false);
+	
+	// Function to Start Game
+	const handleStart = () => {
+		setShow(true);
+	};
+
 	return (
 		<div>
-			<h1>Welcome to Madlibz City</h1>
-            <button>Start Game</button>
+			{!show ? (
+				<div>
+					<h1>Welcome to Madlibz City</h1>
+					<button onClick={handleStart}>Start Game</button>
+				</div>
+			) : (
+				<div>
+					<button>Funny Madlib</button>
+					<button>Sad Madlib</button>
+				</div>
+			)}
 		</div>
 	);
 };
