d56f7123d3cd19bd53bf2f5a0d29bcbdd63df047
Ben Nguyen <bennguyenn24@gmail.com>
2023-01-25 14:39:34 -0800

    Added State and Input Handlers

--- src/components/FunForm.jsx
+++ src/components/FunForm.jsx
@@ -2,15 +2,13 @@ import React from "react";
 import { useState } from "react";
 
 const FunForm = () => {
-	const [index, setIndex] = useState(0);
-	// # Input Handler
 	// Array to store the values
 	const typeArr = [
-		"noun", 
-		"adjective", 
-		"noun (plural)", 
-		"famous person", 
-		"place", 
+		"noun",
+		"adjective",
+		"noun (plural)",
+		"famous person",
+		"place",
 		"verb (ending in ing)",
 		"adjective",
 		"your favorite song",
@@ -19,20 +17,74 @@ const FunForm = () => {
 		"verb (ending in ing)",
 		"name of supermodel",
 		"adjective",
-	]
-	
+	];
+
+	const [index, setIndex] = useState(0);
+	const [count, setCount] = useState(typeArr.length);
+	const [submit, setSubmit] = useState(false);
+
+	// Input State
+	const [word, setWord] = useState("");
+	// Saved Words State
+	const [words, setWords] = useState([]);
+
 	const handleClick = () => {
+		// Save the Word to An Array
+		setWords((words) => [...words, word]);
 
-	}
+		// Clears Input
+		setWord("");
 
- 	return (
+		// Increment the Index
+		setIndex((index) => index + 1);
+
+		// Decrement Count
+		setCount((count) => count - 1);
+	};
+
+	return (
 		<div>
-			<h1>Weird Day Ad-Lib</h1>
-			<form>
-				<p>Enter {typeArr[index]}</p>
-				<input type="text" placeholder={typeArr[index]} />
-			</form>
-			<button>Add Word</button>
+			{!submit ? (
+				<div>
+					<h1>Weird Day Ad-Lib</h1>
+					<div>Words Left: {count}</div>
+					<form>
+						{count != 0 ? (
+							<div>
+								<label htmlFor="word">Enter {typeArr[index]}: </label>
+								<input
+									id="word"
+									name="word"
+									onChange={(e) => setWord(e.target.value)}
+									value={word}
+									type="text"
+									placeholder={typeArr[index]}
+								/>
+								<button type="button" onClick={handleClick}>
+									Add Word
+								</button>
+							</div>
+						) : (
+							<button onClick={() => setSubmit(true)} type="button">
+								Submit
+							</button>
+						)}
+					</form>
+				</div>
+			) : (
+				// Story
+				<div>
+					<p>
+						Once upon a time there was a {words[0]}. It had {words[1]} {words[2]}! One
+						day it met {words[3]} on the side of the
+						{words[4]} they were {words[5]}. It was very {words[6]}; they both looked
+						like hobos! All of the sudden they started singing {words[7]} really
+						loudly. They {words[8]}
+						really {words[9]}! {words[10]} started {words[11]} with {words[12]}. They
+						looked really {words[13]}!
+					</p>
+				</div>
+			)}
 		</div>
 	);
 };
