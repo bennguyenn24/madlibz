79b78ce502a40d0974482dda94d3fb7e946f08d0
Ben Nguyen <bennguyenn24@gmail.com>
2023-01-25 14:38:24 -0800

    Initial Commit

---
+++ src/components/SadForm.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useState } from "react";
+
+const SadForm = () => {
+	const typeArr = () => {
+		
+	}
+	return (
+		<div>
+			
+		</div>
+	);
+};
+
+export default SadForm;
