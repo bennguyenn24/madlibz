64322124f074cc1497aaa880092be27f5532cddc
Ben Nguyen <bennguyenn24@gmail.com>
2023-01-25 14:40:03 -0800

    Added conditional rendering on start

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 import { useState } from "react";
 
-const Start = () => {
+const Start = (props) => {
+	const { setStart, setOptions } = props;
+
 	// Show Available Madlibz
 	const [show, setShow] = useState(false);
-	
-	// Function to Start Game
+
+	// Function to Show Available Madlibz
 	const handleStart = () => {
 		setShow(true);
+	};
+
+	// Function to Start Game
+	const handleClick = (e) => {
+		setStart(true);
+		handleOptions(e);
+	};
+
+	// Function to Handle Options
+	const handleOptions = (e) => {
+		const selected = e.target.innerText;
+		setOptions(selected)
 	};
 
 	return (
@@ -19,8 +33,8 @@ const Start = () => {
 				</div>
 			) : (
 				<div>
-					<button>Funny Madlib</button>
-					<button>Sad Madlib</button>
+					<button onClick={handleClick}>Funny</button>
+					<button onClick={handleClick}>Sad</button>
 				</div>
 			)}
 		</div>
